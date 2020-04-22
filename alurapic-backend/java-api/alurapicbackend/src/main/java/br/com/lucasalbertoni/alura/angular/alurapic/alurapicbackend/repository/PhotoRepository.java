ac6b99c6a12de4a941f793b29c5b68fc69be4571
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-04-22 20:58:29 -0300

    Completando o treinamento de angular parte 1. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- alurapic-backend/java-api/alurapicbackend/src/main/java/br/com/lucasalbertoni/alura/angular/alurapic/alurapicbackend/repository/PhotoRepository.java
+++ alurapic-backend/java-api/alurapicbackend/src/main/java/br/com/lucasalbertoni/alura/angular/alurapic/alurapicbackend/repository/PhotoRepository.java
@@ -3,9 +3,9 @@ package br.com.lucasalbertoni.alura.angular.alurapic.alurapicbackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.lucasalbertoni.alura.angular.alurapic.alurapicbackend.entity.Teste;
+import br.com.lucasalbertoni.alura.angular.alurapic.alurapicbackend.entity.Photo;
 
 @Repository
-public interface PhotoRepository extends JpaRepository<Teste, Long>{
+public interface PhotoRepository extends JpaRepository<Photo, Long>{
 
 }
